f4691feafb7d32e4f56787f16b5501e0a1958d2d
AleksWolski <93227090+AleksWolski@users.noreply.github.com>
2025-12-23 17:19:06 +0000

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: mode === "production" ? "/aleks2.wolski.me.uk/" : "/",
+  base: mode === "production" ? "/" : "/",
   server: {
     host: "::",
     port: 8080,
